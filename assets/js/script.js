75535615a6669ef343b0eca4322c7cd606d38fac
JHON OFICINA <alecsey05car@gmail.com>
2025-05-30 12:31:57 -0500

    ds

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,37 @@ const swiper = new Swiper('.swiper', {
   loop: true,
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: 3, // CORREGIDO
+  slidesPerView: 3, 
   spaceBetween: 30,
 
   coverflowEffect: {
-    rotate: 20,
-    stretch: 0,
+    rotate: 2,
+    stretch: -155,
     depth: 350,
     modifier: 1,
-    slideShadows: true
+    slideShadows: false
   },
 
   autoplay: {
-    delay: 1000,
+    delay: 3000,
     disableOnInteraction: false
   },
 
   pagination: {
     el: ".swiper-pagination",
     clickable: true
+  },
+  
+  breakpoints: {
+  0: {
+    slidesPerView: 1
+  },
+  768: {
+    slidesPerView: 2
+  },
+  1024: {
+    slidesPerView: 3
   }
+}
+
 });
