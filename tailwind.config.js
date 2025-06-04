fb60f2a15af0922667cd2cb09fb0a8cd2d58d44e
JHON OFICINA <alecsey05car@gmail.com>
2025-06-04 16:27:14 -0500

    04-06-2025

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */ module.exports = { content: [ "./*.html", 
-    // Escanea todos los archivos HTML en la raíz (index.html, contacto.html, etc.)
-    "./partials/**/*.html", // Escanea archivos HTML dentro de la carpeta partials 
-    "./assets/js/**/*.js", // Escanea archivos JavaScript en assets/js 
-    "./servicios/**/*.html", // Si tienes HTML dentro de la carpeta servicios 
-    "./servicios/**/*.js", // Si tienes JS dentro de la carpeta servicios
-  ], theme: { extend: {},
+/** @type {import('tailwindcss').Config} */ module.exports = { 
+  content: [
+  "./**/*.html",
+  "./**/*.php",
+  "./**/*.js",
+  "./src/**/*.{js,ts,jsx,tsx}"
+], theme: { extend: {},
   },
   plugins: [],
 }
