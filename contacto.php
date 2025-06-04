b987830db017c23fa7339fd38fc5318797494816
JHON OFICINA <alecsey05car@gmail.com>
2025-06-04 13:34:14 -0500

    modelo 3d moto de pipe

--- contacto.php
+++ contacto.php
@@ -6,6 +6,7 @@
     <title>Nosotros</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 
 </head>
 
@@ -18,42 +19,130 @@
         include_once 'src/inc/navbar.php';
 
 ?>
-       
 
   
      <section id="contacto" class="pt-40 pb-10 bg-blue-50">
-        <div class="container mx-auto px-6">
-            <h2 class="text-3xl font-bold text-center mb-12 text-gray-800">Ponte en Contacto 📧</h2>
-            <div class="max-w-2xl mx-auto bg-gray-50 p-8 rounded-lg shadow-lg">
-                <form action="#" method="POST">
-                    <div class="mb-6">
-                        <label for="name" class="block text-gray-700 font-semibold mb-2">Nombre Completo</label>
-                        <input type="text" name="name" id="name" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+        <div id="contacto" class="pt-16 pb-10 bg-white">
+        <div class="container ml-64 flex items-center justify-right px-6">
+    
+                  <div class="max-w-4xl mx-auto px-4 text-gray-800">
+
+                    <h2 class="text-4xl font-bold text-center  mb-20 text-gray-800">
+                      Contáctanos 
+                    </h2>
+
+                    <h4 class="text-2xl font-bold text-left  mb-4 flex text-gray-800">
+                      <i class="fa-solid fa-location-dot mr-2"></i> &nbsp; Carrera 59 #44a-30, Bogotá, Colombia. 
+                    </h4>
+
+                    <h4 class="text-2xl font-bold text-left  mb-4 text-gray-800">
+                      <i class="fa-regular fa-envelope mr-2"></i> &nbsp; contacto@kasay.com.co 
+                    </h4>
+
+                    <h4 class="text-2xl font-bold text-left  mb-20 text-gray-800">
+                      <i class="fa-solid fa-phone mr-2"></i> &nbsp; 3013365167 
+                    </h4>
+
+                    <h4 class="text-2xl font-bold text-center  mb-4 text-gray-800">
+                      ¡Síguenos en nuestras redes sociales!
+                    </h4>
+                      
+                      <div class="flex justify-center space-x-6 text-3xl">
+
+                        <h4 class="text-2xl font-bold text-left  mb-4 text-gray-800">
+                          <i class="fa-brands fa-square-facebook mr-2"></i>
+                        </h4>
+
+                        <h4 class="text-2xl font-bold text-left  mb-4 text-gray-800">
+                          <i class="fa-brands fa-square-instagram mr-2"></i>
+                        </h4>
+
+                        <h4 class="text-2xl font-bold text-left  mb-4 text-gray-800">
+                          <i class="fa-brands fa-square-x-twitter mr-2"></i>
+                        </h4>
+                        
+                      </div>
+
                     </div>
-                    <div class="mb-6">
-                        <label for="email" class="block text-gray-700 font-semibold mb-2">Correo Electrónico</label>
-                        <input type="email" name="email" id="email" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+        
+          <div class="w-full md:w-1/8" >
+            <form class="bg-blue-50 mr-60 ml-0 px-8 pt-6 pb-8 mb-4 ">
+              
+            <div class="mb-4">
+                <div class="grid grid-flow-row sm:grid-flow-col gap-3">
+                    
+                    <div class="sm:col-span-4 justify-center">
+                      <label class="block text-gray-700 text-sm font-bold mb-2" for="nya">Nombre *</label>
+                      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nya" name="nya" type="text" required placeholder="Ingresa tu nombre ">
                     </div>
-                    <div class="mb-6">
-                        <label for="message" class="block text-gray-700 font-semibold mb-2">Mensaje</label>
-                        <textarea name="message" id="message" rows="5" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required></textarea>
+                    
+                    <div class="sm:col-span-4 justify-center">
+                      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email *</label>
+                      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" name="email" type="text" required placeholder="Ingresa tu email">
                     </div>
-                    <div class="text-center">
-                        <button type="submit" class="bg-blue-600 text-white font-bold py-3 px-8 rounded-lg hover:bg-blue-500 transition duration-300">Enviar Mensaje</button>
+                </div>
+              </div>
+
+              <div class="mb-4">
+                <div class="grid grid-flow-row sm:grid-flow-col gap-3">
+
+                    <div class="sm:col-span-4 justify-center">
+                      <label class="block text-gray-700 text-sm font-bold mb-2" for="telefono">Telefono *</label>
+                      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="telefono" name="telefono" type="text" required placeholder="Ingresa tu telefono ">
                     </div>
-                </form>
-            </div>
+                    
+                    <div class="sm:col-span-4 justify-center">
+                      <label class="block text-gray-700 text-sm font-bold mb-2" for="direccion">Dirección </label>
+                      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="direccion" name="direccion" type="text" required placeholder="Ingresa tu dirección">
+                    </div>
+                </div>
+
+              <div class="mb-4">
+                <div class="grid grid-flow-row sm:grid-flow-col gap-3">
+
+                    <div class="sm:col-span-4 justify-center">
+                      <label class="block text-gray-700 text-sm font-bold mb-2" for="asunto">Asunto *</label>
+                      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="asunto" name="asunto" type="text" required placeholder="Ingresa el asunto ">
+                    </div>
+              
+                </div>
+              </div>
+
+              <div class="mb-4">
+                <div class="grid grid-flow-row sm:grid-flow-col gap-3">
+
+                    <div class="sm:col-span-4 justify-center">
+                      <label class="block text-gray-700 text-sm font-bold mb-2" for="mensaje">Mensaje *</label>
+                      <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="mensaje" name="mensaje" required placeholder="Ingresa tu mesaje "> </textarea>
+                    </div>
+              
+                </div>
+              </div>
+            
+              <div class="flex items-center justify-between">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" >
+                  Enviar
+                </button>
+              
+
+            </form> 
+          </div>
+        </div>
         </div>
     </section>
 
-    <hr/>
+<hr/>
 
-        <section id="nosotros" class="pt-0 bg-white">
-
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.6913261566597!2d-74.09614079005497!3d4.649035495306297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9bb8db53308d%3A0xea102704edc4714b!2sKasay%20Ingenier%C3%ADa!5e0!3m2!1sen!2sco!4v1748981413696!5m2!1sen!2sco" width="1900" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    
-    </section>
-        <hr/>
+  <section id="nosotros" class="pt-0 bg-white">
+    <div class="w-full h-[350px]">
+       
+        <iframe class="w-full h-30 border-0" 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.6913261566597!2d-74.09614079005497!3d4.649035495306297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9bb8db53308d%3A0xea102704edc4714b!2sKasay%20Ingenier%C3%ADa!5e0!3m2!1sen!2sco!4v1748981413696!5m2!1sen!2sco" width="1900" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+      
+    </div>
+  </section>        
+<hr/>
 
  <!-- Pie de pagina  -->
  <?php
---
+++ modelo3d.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nosotros</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+    <link href="https://cesium.com/downloads/cesiumjs/releases/1.126/Build/Cesium/Widgets/widgets.css" rel="stylesheet">
+        <style>
+  
+    model-viewer {
+        width: 800px;
+        height: 450px;
+        background-color: #2c2c2c;
+    }
+    </style>
+</head>
+
+
+<body class="bg-gray-100 text-gray-800">
+
+<!-- Barra de navegación -->
+ <?php
+
+        include_once 'src/inc/navbar.php';
+
+?>
+       
+
+  
+<section id="contacto" class="pt-28 bg-blue-50">
+  <div class="w-full px-20">
+    <div class="bg-white p-8 rounded-lg">
+      
+      <h2 class="text-3xl font-bold text-center mb-12 text-gray-800">Modelo 3D Xd</h2>
+
+     
+      <div class="flex flex-col md:flex-row items-center gap-8">
+        
+      
+        <model-viewer
+          class="rounded-lg shadow-lg mb-20 ml-8" 
+          src="moto.glb"
+          alt="Modelo 3D de un terreno"
+          auto-rotate
+          camera-controls
+          shadow-intensity="1"
+          max-camera-distance="10m"
+          camera-orbit="10deg -190deg 0.3m"
+          ar>
+        </model-viewer>
+
+    
+        <div class="w-full md:w-1/2 text-gray-800">
+  <h2 class="text-2xl font-bold mb-4">La mejor moto</h2>
+  <p class="text-gray-700">
+    Descubre la <strong>Yamaha MT-15</strong>, una fusión perfecta entre <em>diseño agresivo</em>, <em>tecnología de punta</em> y <em>rendimiento urbano</em>. 
+    Esta máquina ligera pero poderosa está hecha para quienes viven con pasión cada curva y disfrutan la ciudad con actitud. <br><br>
+    Explora cada detalle en 3D y siente de cerca su <strong>estilo naked</strong>, su motor de alto desempeño y su esencia rebelde. 
+    ¿Estás listo para dominar las calles?
+  </p>
+</div>
+
+
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section id="contacto" class="pt-8 bg-blue-50">
+  <div class="w-full px-20">
+    <div class="bg-white p-8 rounded-lg">
+      
+      <h2 class="text-3xl font-bold text-center mb-12 text-white">Modelo 3D Xd</h2>
+
+     
+      <div class="flex flex-col md:flex-row items-center gap-8">
+        
+              <div class="w-full md:w-1/2 text-gray-800">
+          <h2 class="text-2xl font-bold mb-4">figura</h2>
+          <p class="text-gray-700">
+           Como es?
+          </p>
+        </div>
+
+        <model-viewer
+          class="rounded-lg shadow-lg mb-20 ml-8" 
+          src="modelos.glb"
+          alt="Modelo 3D de un terreno"
+          auto-rotate
+          camera-controls
+          shadow-intensity="1"
+          max-camera-distance="100m"
+          camera-orbit="0deg 75deg 100m"
+          ar>
+        </model-viewer>
+
+    
+
+
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section id="contacto" class="pt-8 pb-10 bg-blue-50">
+  <div class="w-full px-20">
+    <div class="bg-white p-8 rounded-lg">
+      
+      <h2 class="text-3xl font-bold text-center mb-12 text-gray-800">Modelo 3D Xd</h2>
+
+     
+      <div class="flex flex-col md:flex-row items-center gap-8">
+        
+      
+        <model-viewer
+          class="rounded-lg shadow-lg mb-20 ml-8" 
+          src="1.glb"
+          alt="Modelo 3D de un terreno"
+          auto-rotate
+          camera-controls
+          shadow-intensity="1"
+          max-camera-distance="100m"
+          camera-orbit="0deg 75deg 8m"
+          ar>
+        </model-viewer>
+
+    
+        <div class="w-full md:w-1/2 text-gray-800">
+          <h2 class="text-2xl font-bold mb-4">arma</h2>
+          <p class="text-gray-700">
+           Como es?
+          </p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
+
+
+    <hr/>
+
+
+
+ <!-- Pie de pagina  -->
+ <?php
+
+        include_once 'src/inc/footer.php';
+ ?>
+
+</body>
+</html>
